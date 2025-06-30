6a896d9f253605c6f4ee1c78dac6a839a5109100
yadhvi13 <yadhvi13@gmail.com>
2025-06-30 10:55:25 +0530

    error Handling

--- JavaScript/Error Handling/README.md
+++ JavaScript/Error Handling/README.md
@@ -5,3 +5,10 @@
 #types of errors
 1. Run time - while the code is executing, error that time caught.
 2. compile time - caught during parse time
+
+#Handling
+1.) Try-catch block
+2.) finally block - this will run wheather the error is there pr not
+
+#if ap khudse koi error return krna chahe, toh use THROW keyword
+3.) throw
--- JavaScript/Error Handling/script.js
+++ JavaScript/Error Handling/script.js
@@ -4,4 +4,44 @@
 
 //RUN TIME error
 //As x is not defined, so it will take error in run time
-console.log(x);
+// console.log(x);
+
+// try{
+//     //jisme error aa ksta hai yha likho
+//     console.log("try block start here");   
+//     //refernce error ayega yha
+//   console.log(x);  
+//   console.log("try block ends here");
+  
+// }
+// catch(e){
+// //agr error aayega toh use yha catch kr lega ye block
+// //error ke sath kya krna hai
+// //1.) Retry logic
+// //2.) fallback mechanism
+// console.log("I am inside console block");
+// console.log("YOUR ERROR IS HERE: ", e);
+
+// }
+// finally{
+//   console.log("i will run everytime as i am finally block");
+  
+// }
+
+
+//LETS CREATE CUSTOM ERROR
+// try{
+//   //refrence error
+//   console.log(x);
+  
+// }
+// catch(err){
+//   //khudka error throw krna
+//    throw new Error("Phle declare kro fr use krna");
+// }
+
+
+let errorCode = 100;
+if(errorCode == 100){
+   throw new Error("Invalid JSON");
+}
